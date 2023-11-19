208ed6be2a51b5e400de07c4419bd108013bea14
Marco Zanon <marco.zanon@pm.me>
2023-11-19 14:46:28 +0100

    fix project root for live grep

--- init.lua
+++ init.lua
@@ -242,10 +242,15 @@ require('telescope').setup {
 -- lsp active in the current buffer
 local project_root = function()
   local cwd = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
-  if vim.v.shell_error ~= 0 then
-    -- if not git then active lsp client root
+
+  -- if not git then active lsp client root
+  if vim.v.shell_error ~= 0 and vim.lsp.get_active_clients()[1] ~= nil then
     -- will get the configured root directory of the first attached lsp. You will have problems if you are using multiple lsps 
     cwd = vim.lsp.get_active_clients()[1].config.root_dir
+
+  -- if not lsp, then the cwd
+  else
+    cwd = vim.loop.cwd()
   end
   return cwd
 end
