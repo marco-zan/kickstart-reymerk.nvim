9fdcc4074ee2dc791434550380431c380382cc53
Marco Zanon <marco.zanon@pm.me>
2026-08-08 15:29:55 +0200

    Updated neovide settings

--- lua/reymerk/settings_neovide.lua
+++ lua/reymerk/settings_neovide.lua
@@ -1,7 +1,7 @@
 
 if vim.g.neovide then
-  vim.o.guifont = "CaskaydiaCove NFM:h12"
-  vim.g.neovide_normal_opacity = 0.8
+  vim.o.guifont = "Caskaydia Cove:h12"
+  vim.g.neovide_normal_opacity = 0.9
 
   vim.g.neovide_scroll_animation_length = 0.1
   vim.g.neovide_scroll_animation_far_lines = 0
@@ -12,10 +12,12 @@ if vim.g.neovide then
   vim.g.neovide_cursor_trail_size = 0.2
   vim.g.neovide_cursor_animation_length = 0.08
 
+  local term_cmd = "terminal /usr/bin/env fish"
+
 
   -- [[ GUI Keymaps ]]
   vim.keymap.set('n', '<M-t>', function ()
-    vim.cmd("tabnew|terminal /usr/bin/env fish")
+    vim.cmd("tabnew|" .. term_cmd)
   end, { desc = "[D]iagnostics [D]iagnostics cursor position" })
 
   for i = 1, 9 do
@@ -34,15 +36,13 @@ if vim.g.neovide then
   -- Move the current tab
   -- ghostty: alt+period=move_tab:-1
   vim.keymap.set(
-    'n',
-    '<M-.>',
+    'n', '<M-.>',
     '<Cmd>tabmove -1<CR>',
     { desc = 'Move tab left', silent = true }
   )
   -- ghostty: alt+comma=move_tab:+1
   vim.keymap.set(
-    'n',
-    '<M-,>',
+    'n', '<M-,>',
     '<Cmd>tabmove +1<CR>',
     { desc = 'Move tab right', silent = true }
   )
@@ -53,43 +53,39 @@ if vim.g.neovide then
   -- Create new terminal splits
   -- ghostty: alt+p=new_split:right
   vim.keymap.set('n', '<M-p>', function()
-    vim.cmd('vsplit | terminal')
+    vim.cmd("vsplit |" .. term_cmd)
   end, { desc = 'New terminal split right' })
   -- ghostty: alt+shift+p=new_split:left
   vim.keymap.set('n', '<M-S-p>', function()
-    vim.cmd('leftabove vsplit | terminal')
+    vim.cmd("leftabove vsplit |" .. term_cmd)
   end, { desc = 'New terminal split left' })
   -- ghostty: alt+-=new_split:down
   vim.keymap.set('n', '<M- ->', function()
-    vim.cmd('split | terminal')
+    vim.cmd("split |" .. term_cmd)
   end, { desc = 'New terminal split down' })
 
   -- Navigate between splits
   -- ghostty: alt+l=goto_split:right
   vim.keymap.set(
-    'n',
-    '<M-l>',
+    'n', '<M-l>',
     '<C-w>l',
     { desc = 'Go to split right', silent = true }
   )
   -- ghostty: alt+h=goto_split:left
   vim.keymap.set(
-    'n',
-    '<M-h>',
+    'n', '<M-h>',
     '<C-w>h',
     { desc = 'Go to split left', silent = true }
   )
   -- ghostty: alt+j=goto_split:bottom
   vim.keymap.set(
-    'n',
-    '<M-j>',
+    'n', '<M-j>',
     '<C-w>j',
     { desc = 'Go to split down', silent = true }
   )
   -- ghostty: alt+k=goto_split:top
   vim.keymap.set(
-    'n',
-    '<M-k>',
+    'n', '<M-k>',
     '<C-w>k',
     { desc = 'Go to split up', silent = true }
   )
